c1782601b8361bca4f051049f23067758e64fe1a
Romeu Leite <119366384+romeuleite@users.noreply.github.com>
2023-10-31 04:22:32 -0300

    Update uart.cpp

--- uart.cpp
+++ uart.cpp
@@ -2,7 +2,69 @@
 
 void UART_RX::put_samples(const unsigned int *buffer, unsigned int n)
 {
-    // seu código aqui
+    static unsigned int bit_counter = 0; 
+    static unsigned int noisy_counter = 0;
+    static uint8_t byte = 0;
+
+    //Máquina de estados 
+    static enum class State {
+        SINCRONIZING,
+        RECEIVING,
+        WAITING
+    } state = State::WAITING;
+
+    for (unsigned int i = 0; i < n; i++) {
+        unsigned int sample = buffer[i];
+
+        switch (state) {
+            case State::WAITING:
+                if (sample == 0) {
+                    state = State::SINCRONIZING;
+                }
+            break;
+
+            // Verifica se 25 dentre as ultimas 30 amostras 
+            // lidas possuem nível lógico baixo
+            case State::SINCRONIZING:
+                bit_counter++;
+
+                if (sample == 0) {
+                    if (bit_counter == 30) {
+                        state = State::RECEIVING;
+                        byte = 0;
+                        noisy_counter = 0;
+                    }
+                } else {
+                    noisy_counter++;
+
+                    if (noisy_counter > 5) {
+                        state = State::WAITING;
+                        bit_counter = 0;
+                        noisy_counter = 0;
+                    }
+                }
+
+            break;
+
+            // Lê um bit de dado a cada 160 amaostras, iniciando a
+            // partir do bit_counter = 80 (meio do primeiro intervalo de dados)
+            case State::RECEIVING:
+                bit_counter++;
+
+                if ((bit_counter % SAMPLES_PER_SYMBOL - (SAMPLES_PER_SYMBOL / 2)) == 0) { 
+                    byte |= (sample << (bit_counter / SAMPLES_PER_SYMBOL - 1));
+
+                    if (bit_counter >= (9 * SAMPLES_PER_SYMBOL)) {
+                        state = State::WAITING;
+                        bit_counter = 0;
+                        noisy_counter = 0;
+                        get_byte(byte);
+                    }
+                }
+
+            break;
+        }
+    }
 }
 
 void UART_TX::put_byte(uint8_t byte)
